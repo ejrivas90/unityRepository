6a61cbd781fcadbc7dadfa6cc7c505367abca256
ejrivas90 <ever.4everstudios@yahoo.com>
2016-09-29 12:58:14 -0700

    unity project check in

---
+++ The Battle Front/Assets/scripts/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseController : MonoBehaviour {
+    private int baseHealth;
+
+	// Use this for initialization
+	void Start () {
+        baseHealth = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    int takeDamage(int damageTaken) {
+        int newHealth = this.baseHealth - damageTaken;
+        return newHealth;
+    }
+}
---
+++ The Battle Front/Assets/scripts/PlayerStateMachine.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStateMachine : MonoBehaviour {
+    public PlayerController player;
+    public TurnState currentState;
+
+    public enum TurnState {
+        ATTACK,
+        MOVE,
+        WAIT,
+        SUMMON,
+        DEAD,
+        TAKE_DAMAGE,
+    }   
+
+	void Start () {
+	    
+	}
+	
+	
+	void Update () {
+        switch (currentState) {
+            case (TurnState.ATTACK):
+                break;
+            case (TurnState.DEAD):
+                break;
+            case (TurnState.MOVE):
+                break;
+            case (TurnState.SUMMON):
+                break;
+            case (TurnState.WAIT):
+                break;
+            case (TurnState.TAKE_DAMAGE):
+                break;
+        }         
+	}
+
+    void updatePlayerHealth() {
+
+    }
+}
